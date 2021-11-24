8bebfa897b9050b3ccedc18ab3502cdc6a73a406
June Kim <zhxxne@gmail.com>
2021-11-24 15:18:21 +0100

    Book Interface and Dashboard Component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,37 @@
+import { Book } from './../shared/book';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  // books?: Book[];
+  // books : Book[] | undefined;
+  books: Book[] = [];
+
+  constructor() { // From Angular 12, not in ngOnInit, but in constructor; Strict blah..
+    this.books = [
+      {
+        isbn: '111',
+        title: 'Angular',
+        description: 'Angular AtoZ',
+        rating: 5,
+        price: 38.90
+      },
+      {
+        isbn: '222',
+        title: 'TypeScript',
+        description: 'TypeScript AtoZ',
+        rating: 4,
+        price: 25.90
+      }
+    ];
+   }
+
+  ngOnInit(): void { // LifeCycle Hook; Handle when the component starts
+  }
+
+}
---
+++ src/app/shared/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  isbn: string;
+  title: string;
+  // authors: string[]; // TODO
+  description: string;
+  price: number;
+  rating: number;
+}
