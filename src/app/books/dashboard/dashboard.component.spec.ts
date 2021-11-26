b8e5e8ad82955890ccb31d7e1c0b2242b02814b7
June Kim <zhxxne@gmail.com>
2021-11-26 12:01:23 +0100

    Shallow Component Tests

--- src/app/books/dashboard/dashboard.component.spec.ts
+++ src/app/books/dashboard/dashboard.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Book } from '../shared/book';
 
 import { DashboardComponent } from './dashboard.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -24,6 +25,7 @@ describe('DashboardComponent', () => {
 
     await TestBed.configureTestingModule({
       declarations: [ DashboardComponent ],
+      schemas: [NO_ERRORS_SCHEMA], // Shallow Component Test: all child components are ignored
       providers: [
         { provide: BookRatingService, useValue: ratingMock }
       ]
