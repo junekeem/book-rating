15c4c70e51074ea994f1fb20aa3b1bc7af098874
June Kim <zhxxne@gmail.com>
2021-11-29 16:23:58 +0100

    Freiwillige Hausaufgabe (4)
    - Nutze die DatePipe, um das aktuelle Datum in der Anwendung anzuzeigen.
    - Zeige auch die Uhrzeit an und aktualisiere die Zeit regelmäßig.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { map, Observable, shareReplay, timer } from 'rxjs';
 
 @Component({
   selector: 'br-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Book Rating!';
+
+  time: Observable<Date> = timer(0, 1000).pipe(
+    map(() => new Date()),
+    shareReplay(1)
+  );
+  
+  // https://stackoverflow.com/questions/54289078/display-time-clock-in-angular
 }
